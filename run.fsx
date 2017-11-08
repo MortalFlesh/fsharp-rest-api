45f68b38d78a3a96226dad84de8440c2fbd27c9a
Petr Chromec <mortal_flesh@seznam.cz>
2017-11-09 00:22:35 +0100

    Profile added

--- run.fsx
+++ run.fsx
@@ -5,7 +5,7 @@ open System
 #I @"/Users/chromecp/.nvm/versions/node/v8.4.0/bin/"
 
 #r "Microsoft.Azure.Webjobs.Host.dll"
-open Microsoft.Azure.WebJobs.Host
+//open Microsoft.Azure.WebJobs.Host
 
 #r "System.Net.Http.Formatting.dll"
 #r "System.Web.Http.dll"
@@ -19,12 +19,24 @@ open Microsoft.Azure.WebJobs.Host
 
 #endif
 
+// ^^^ code above is for local development only ^^^
+
 open System.Text
 open System.Net
 open System.Net.Http
 
-let Run(req: HttpRequestMessage, template: string, name: string) =
-  let html = template.Replace("%name%", name)
+type Mark = string
+type Value = string
+type Template = string
+
+let mark (mark: Mark) (value: Value) (template: Template) =
+  template.Replace(sprintf "{{ %s }}" mark, value)
+
+let Run(req: HttpRequestMessage, template: Template, name: Value) =
+  let html =
+    template
+      |> mark "title" "Profile"
+      |> mark "name" name
   
   let response = req.CreateResponse(HttpStatusCode.OK)
   response.Content <- new StringContent(html, Encoding.UTF8, "text/html")
